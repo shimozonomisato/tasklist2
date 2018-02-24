6f89933f7cdb2d57e35b374efbbc3f0e12239d01
zono <>
2018-02-24 18:11:27 +0900

    tasks2テーブル作成・マイグレーション

--- laravel/database/migrations/2018_02_22_123616_create_tasks2_table.php
+++ laravel/database/migrations/2018_02_22_123616_create_tasks2_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTasksTable extends Migration
+class CreateTasks2Table extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
+        Schema::create('tasks2', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->index();
             $table->string('name');
@@ -28,6 +28,6 @@ class CreateTasksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('tasks2');
     }
 }
