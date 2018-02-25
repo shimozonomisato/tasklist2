d805348a67479b6e0cb557e738cdd37ac48ff744
zono <>
2018-02-25 13:02:00 +0900

    リレーションを定義

--- laravel/app/Task2.php
+++ laravel/app/Task2.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Task2 extends Model
 {
     protected $fillable = ['name'];
+
+    /*タスク所有ユーザーの取得*/
+    public function user()
+    {
+      return $this->belongsTo(user::class);
+    }
 }
