cefab82250fac456a2ecf368851ef03847993bc9
zono <>
2018-03-03 16:10:41 +0900

    重複解消

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -15,24 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//認証ルート
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
-
-//登録ルート
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
-
 //task2コントローラーへのルート
-Route::get('/tasks', 'Task2Controller@index');
-Route::post('/task', 'Task2Controller@store');
+Route::get('/tasks', 'TaskController@index');
+Route::post('/task', 'TaskController@store');
 Route::delete('/task/{task}', 'Task2Controller@destroy');
 
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
